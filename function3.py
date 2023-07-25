47703e5e27f1fb454a42ea5fbb9fee677584ed6a
wjdgks85 <wjdgks9785@gmail.com>
2023-07-25 12:46:17 +0900

    수정 및 신규

--- function3.py
+++ function3.py
@@ -13,3 +13,22 @@ def union(*ar):
 # 호출
 print(union("HAM","SPAM"))
 print(union("HAM",'SPAM','EGG'))
+
+# 내장함수
+#필터링 함수 정의
+def getBiggerThan20(i):
+    return i>20
+
+lst=[10,25,30]
+iterL=filter(None,lst)
+for i in iterL:
+    print(i)
+
+iterL2=filter(getBiggerThan20,lst)
+for k in iterL2:
+    print(k)
+
+print("---람다함수---")
+iterL=filter(lambda x:x>20,lst)
+for i in iterL:
+    print(i)
