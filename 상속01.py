9f998ea5d08f75b1c3110e6a7e3c45bf04474324
wjdgks85 <wjdgks9785@gmail.com>
2023-07-26 09:34:34 +0900

    파일 연습1

--- 상속01.py
+++ 상속01.py
@@ -6,17 +6,27 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print("현재 코딩중")
+
 class Student(Person):
+    # 상속 받고 덮어쓰기 (재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        # 명시적으로 부모클래스 초기화 메서드 호출
+        Person.__init__(self,name,phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject):{0}, StudentID:{1})".format(self.subject,self.studentID))
 
-
+# 인스턴스 생성
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
-
-
+s = Student("이순신", "010-111-1234", "빅데이터", "201234")
+p.printInfo()
+s.printInfo()
+s.coding()
+s.working()
